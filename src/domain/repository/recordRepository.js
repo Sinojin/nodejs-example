9d9f1e2a59e5d2b46e68e65b6ba82fa66c40ff2b
Emirhan Dinç <emirhan.dinc@spinmatic.com>
2019-08-22 04:02:30 +0300

    recordRepository added

---
+++ src/domain/repository/recordRepository.js
@@ -0,0 +1,15 @@
+//  This Repository class define border for any database and It will be bridge to access data
+// We can use this class like bridge and you can add new log or cache system into middle of real database and there is no code change will require
+
+class RecordRepository {
+    constructor (repository) {
+      this.repository = repository
+    }
+  
+    async getRecordsBy (startDate, endDate, minCount, maxCount) {
+      return this.repository.getRecordsBy(startDate, endDate, minCount, maxCount)
+    }
+  }
+  
+  module.exports = RecordRepository
+  
